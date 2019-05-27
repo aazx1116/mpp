a87b093214cbfef09acd5a9b45b544260bca9599
jungbin <docry_@naver.com>
2019-05-27 22:57:16 +0900

    5.27

--- app/src/main/java/com/example/mpp/MainActivity.java
+++ app/src/main/java/com/example/mpp/MainActivity.java
@@ -1,150 +1,25 @@
 package com.example.mpp;
 
-
-import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.view.View.OnClickListener;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.GridView;
-import android.widget.TextView;
-
-import org.xml.sax.ContentHandler;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
-
-
 
 public class MainActivity extends AppCompatActivity {
-    LinearLayout container;
-    private TextView tvDate;
-    private GridAdapter gridAdapter;
-    private ArrayList<String> dayList;
-    private GridView gridView;
-    private Calendar mCal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        container = (LinearLayout) findViewById(R.id.container);
-        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        inflater.inflate(R.layout.customlayout, container, true);
 
-        Button buttonMenu = (Button) findViewById(R.id.btnMenu);
-
-        buttonMenu.setOnClickListener(new View.OnClickListener() {
-            @Override
+        Button btnList = (Button) findViewById(R.id.btnList);
+        btnList.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), setting.class);
+                Intent intent = new Intent(getApplicationContext(), Monthly.class);
                 startActivity(intent);
             }
         });
-        tvDate = (TextView)findViewById(R.id.tv_date);
-        gridView = (GridView)findViewById(R.id.gridview);
-        long now = System.currentTimeMillis();
-
-        final Date date = new Date(now);
-        final SimpleDateFormat curYearFormat = new SimpleDateFormat("yyyy", Locale.KOREA);
-        final SimpleDateFormat curMonthFormat = new SimpleDateFormat("MM", Locale.KOREA);
-        final SimpleDateFormat curDayFormat = new SimpleDateFormat("dd", Locale.KOREA);
-
-        tvDate.setText(curYearFormat.format(date) + "/" + curMonthFormat.format(date));
-
-
-        dayList = new ArrayList<String>();
-        dayList.add("일");
-        dayList.add("월");
-        dayList.add("화");
-        dayList.add("수");
-        dayList.add("목");
-        dayList.add("금");
-        dayList.add("토");
-
-        mCal = Calendar.getInstance();
-
-        mCal.set(Integer.parseInt(curYearFormat.format(date)), Integer.parseInt(curMonthFormat.format(date)) - 1, 1);
-
-        int dayNum = mCal.get(Calendar.DAY_OF_WEEK);
-
-        for (int i = 1; i < dayNum; i++) {
-            dayList.add("");
-        }
-
-        setCalendarDate(mCal.get(Calendar.MONTH) + 1);
-
-        gridAdapter = new GridAdapter(getApplicationContext(), dayList);
-        gridView.setAdapter(gridAdapter);
-
-
-    }
-
-    private void setCalendarDate(int month) {
-        mCal.set(Calendar.MONTH, month - 1);
-        for (int i = 0; i < mCal.getActualMaximum(Calendar.DAY_OF_MONTH); i++) {
-            dayList.add("" + (i + 1));
-        }
-    }
-
-    private class GridAdapter extends BaseAdapter {
-        private final List<String> list;
-        private final LayoutInflater inflater;
-
-
-        public GridAdapter(Context context, List<String> list) {
-            this.list = list;
-            this.inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        }
-
-        @Override
-        public int getCount() {
-            return list.size();
-        }
-
-        @Override
-        public String getItem(int position) {
-            return list.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            ViewHolder holder = null;
-            if (convertView == null) {
-                convertView = inflater.inflate(R.layout.item_calendar_gridview, parent, false);
-                holder = new ViewHolder();
-                holder.tvItemGridView = (TextView)convertView.findViewById(R.id.tv_item_gridview);
-                convertView.setTag(holder);
-            } else {
-                holder = (ViewHolder)convertView.getTag();
-            }
-            holder.tvItemGridView.setText("" + getItem(position));
-            mCal = Calendar.getInstance();
-            Integer today = mCal.get(Calendar.DAY_OF_MONTH);
-            String sToday = String.valueOf(today);
-            if (sToday.equals(getItem(position))) {
-                holder.tvItemGridView.setTextColor(getResources().getColor(R.color.colorPrimary));
-            }
-            return convertView;
-        }
-    }
-
-    private class ViewHolder {
-        TextView tvItemGridView;
     }
 }
 
--- app/src/main/java/com/example/mpp/setting.java
+++ app/src/main/java/com/example/mpp/setting.java
@@ -2,21 +2,12 @@ package com.example.mpp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 
 public class setting extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.setting);
-
-        Button buttonReturn = (Button) findViewById(R.id.btnReturn);
-        buttonReturn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                finish();
-            }
-        });
+        setContentView(R.layout.Weekly);
     }
 }
