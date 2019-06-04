a3002c75eced2e0df81bebdde26f69908b70c8e8
RH-GON <gorhs0918@gmail.com>
2019-06-04 19:41:25 +0900

    User, Note add

--- app/src/main/java/com/example/mpp/User.java
+++ app/src/main/java/com/example/mpp/User.java
@@ -1,19 +1,46 @@
 package com.example.mpp;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Map;
 
 public class User {
 
-    public void readNote(int noteID)
+    public void readNote(String noteName)
     {
 
     }
-    public void getMemo(int noteID, SimpleDateFormat form, SimpleDateFormat to)
+    public void getMemo(String noteName, SimpleDateFormat form, SimpleDateFormat to)
     {
 
     }
 
-    public void addNote()
+    public void addNote(String noteName, ArrayList<String> description)
+    {
+
+    }
+
+    public void addMemo(String noteName, Map<String,String> contents)
+    {
+
+    }
+    public void updateNote(String noteName, ArrayList<String> description)
+    {
+
+    }
+    public void updateMemo(String notename, Map<String,String> contents)
+    {
+
+    }
+    public void deleteNote(String noteName)
+    {
+
+    }
+    public void deleteMemo(String memoID)
+    {
+        
+    }
+
 
 
 
