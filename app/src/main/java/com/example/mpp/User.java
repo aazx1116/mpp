90630607f2e194f4268a64337997a8c3824b7be7
RH-GON <gorhs0918@gmail.com>
2019-06-04 19:55:22 +0900

    User, Note add

--- app/src/main/java/com/example/mpp/User.java
+++ app/src/main/java/com/example/mpp/User.java
@@ -6,42 +6,44 @@ import java.util.Map;
 
 public class User {
 
-    public void readNote(String noteName)
+    static public void readNote(String noteName)
     {
 
     }
-    public void getMemo(String noteName, SimpleDateFormat form, SimpleDateFormat to)
-    {
-
-    }
-
-    public void addNote(String noteName, ArrayList<String> description)
+    static public void getMemo(String noteName, SimpleDateFormat form, SimpleDateFormat to)
     {
 
     }
 
-    public void addMemo(String noteName, Map<String,String> contents)
+    static public void addNote(String noteName, ArrayList<String> description)
     {
 
     }
-    public void updateNote(String noteName, ArrayList<String> description)
+
+    static public void addMemo(String noteName, Map<String,String> contents)
     {
 
     }
-    public void updateMemo(String notename, Map<String,String> contents)
+    static public void updateNote(String noteName, ArrayList<String> description)
     {
 
     }
-    public void deleteNote(String noteName)
+    static public void updateMemo(String notename, Map<String,String> contents)
     {
 
     }
-    public void deleteMemo(String memoID)
+    static public void deleteNote(String noteName)
+    {
+
+    }
+    static public void deleteMemo(String memoID)
+    {
+
+    }
+    static public ArrayList<String> noteList()
     {
         
     }
-
-
 
 
 }
