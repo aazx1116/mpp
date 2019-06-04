47be82b7fa6023ce16b5883427d939517c4979c1
RH-GON <gorhs0918@gmail.com>
2019-06-04 19:23:51 +0900

    User, Note add

--- app/src/main/java/com/example/mpp/User.java
+++ app/src/main/java/com/example/mpp/User.java
@@ -1,6 +1,20 @@
 package com.example.mpp;
 
+import java.text.SimpleDateFormat;
+
 public class User {
 
-      
+    public void readNote(int noteID)
+    {
+
+    }
+    public void getMemo(int noteID, SimpleDateFormat form, SimpleDateFormat to)
+    {
+
+    }
+
+    public void addNote()
+
+
+
 }
