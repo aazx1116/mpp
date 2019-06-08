14879639d3e1b8209218634e6fcd5b0f5ad2638e
RH-GON <gorhs0918@gmail.com>
2019-06-09 06:57:45 +0900

    Note Done

--- app/src/main/java/com/example/mpp/Note.java
+++ app/src/main/java/com/example/mpp/Note.java
@@ -1,4 +1,176 @@
 package com.example.mpp;
 
-public class Note {
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+
+public class  Note{
+    private SQLiteDatabase myBag;
+    private String noteName;
+    private ArrayList<String> description;
+
+    public Note(String noteName)
+    {
+        this.noteName = noteName;
+        this.myBag = SQLiteDatabase.openOrCreateDatabase("myBag",null);
+        this.getDescription();
+
+    }
+
+    private void updateDescription(ArrayList<String> newDescription)
+    {
+        this.description = newDescription;
+    }
+
+    private ArrayList<String> getDescription()
+    {
+        ArrayList<String> result= new ArrayList();
+
+        Cursor temp =  myBag.rawQuery("SELECT sql FROM sqlite_master WHERE name=\'" + noteName + "\'",null);
+        for(int i = 0 ; !temp.isAfterLast();i++)
+        {
+            result.add(temp.getString(i));
+        }
+
+     return result;
+    }
+    public Cursor open()
+    {
+         return this.findMemo(null,null);
+    }
+
+    public int writeMemo(ArrayList<String> contexts)
+    {
+        String descriptionStr = "";
+        String contextStr = "";
+        int numberOfContext = description.size();
+
+        descriptionStr+="(";
+        for(int  i = 0 ; i < numberOfContext;i++)
+        {
+            descriptionStr +=description.get(i);
+            if(i != numberOfContext - 1)
+                descriptionStr +=",";
+        }
+        descriptionStr+=")";
+        contextStr+="(";
+        for(int  i = 0 ; i < numberOfContext;i++)
+        {
+            contextStr +=description.get(i);
+            if(i != numberOfContext - 1)
+                contextStr +=",";
+        }
+        contextStr+=")";
+        try{
+            myBag.execSQL("INSERT INTO "+noteName+descriptionStr+ "VALUES" +contextStr);
+        }
+        catch(Exception e)
+        {
+            return 0;
+        }
+        return 1;
+    }
+
+    public boolean discardNote()
+    {
+        try {
+            myBag.execSQL("DROP TABLE IF EXISTS \''" + noteName + "\''");
+        }
+        catch(Exception e)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public Cursor findMemo(ArrayList<String> field,ArrayList<String> options)
+    {
+        String fieldStr ="";
+        String optionStr ="";
+        if(field == null)
+        {
+            fieldStr = "*";
+        }
+        else
+        {
+            for(int  i = 0 ; i < field.size();i++)
+            {
+                fieldStr +=field.get(i) ;
+                if(i!=field.size() -1)
+                    fieldStr +=",";
+            }
+        }
+        if(options == null)
+        {
+            return myBag.rawQuery("SELECT " + fieldStr +"FROM" + noteName,null);
+        }
+        else
+        {
+            for(int i = 0 ; i < options.size();i++)
+            {
+                optionStr +=options.get(i);
+            }
+
+            return myBag.rawQuery("SELECT " + fieldStr +"FROM" + noteName + "WHERE" + optionStr,null);
+        }
+
+    }
+
+    public int retreiveNote(ArrayList<String> newDescription)
+    {
+        try {
+            for(int i = 0 ; i < newDescription.size();i++)
+            {
+                if(!description.contains(newDescription.get(i)))
+                    myBag.execSQL("ALTER TABLE" + noteName +"ADD COLUMN" + newDescription.get(i) + "TEXT");
+            }
+        }
+        catch(Exception e)
+        {
+            return 0;
+        }
+        this.description = newDescription;
+        return 1;
+    }
+    public int retreiveMemo(String createdTime,ArrayList<String> newContents)
+    {
+        String setStr = null;
+        String descriptionTmp;
+        String contentsTmp;
+
+        int numberOfDescription = description.size();
+
+        for(int i = 0 ; i < numberOfDescription ;i++)
+        {
+            if(description.get(i) == null)
+                descriptionTmp = "";
+            else
+                descriptionTmp = description.get(i);
+            if(newContents.get(i) == null)
+                contentsTmp = "-";
+            else
+                contentsTmp = newContents.get(i);
+            setStr += descriptionTmp + "=" +contentsTmp;
+            if(i != numberOfDescription - 1)
+                setStr +=",";
+        }
+
+        try {
+            myBag.execSQL("UPDATE " +noteName+"SET" + setStr);
+        }
+        catch (Exception e)
+        {
+            return 0;
+        }
+        return 1;
+    }
+
+
+    public class Memo{
+        ArrayList<String> descriptions;
+        ArrayList<String> attributes;
+        ArrayList<String> contents;
+    }
 }
